bc1bec6724e2e683173078834f3070fde92eda7e
Mark Ching <manho445@gmail.com>
2022-11-12 18:39:29 +0800

    login System&&README

---
+++ README.md
@@ -0,0 +1,32 @@
+Project: BookWell
+Group No: 24
+Group Member: 1[Ching Man Ho, 13019210] 2[,13070826]
+
+User Guide:
+
+User:
+Step 1: User login and access our server
+Step 2: after login user will be go to home page
+##Display books and find books
+Step 3: Server will display list of book for user, also user allows to search a books.
+
+Admin:
+Access System:
+Step 1: Admin user select admin login button at login page to access admin page.
+Step 2: after login admin user will be go to the admin page to manage book details.
+
+Add book:
+Step 1: Admin click the add book button to create page create a new book.
+Step 2: Admin input the book information in the fields. 
+Step 3: Click the "Submit" Button.
+
+update books:
+Step 1: Admin select a book of the list and go to the info page.
+Step 2: Admin click the update book button and go to the update page.
+Step 3: Admin input the new data to each field.
+Step 4: Click the "Submit" Button.
+
+delete book:
+Step 1: Admin select a book of the list and go to the info page.
+Step 2: Admin click the delete book to delete book data.
+Step 3: Click the "Submit" Button.
--- server.js
+++ server.js
@@ -1,51 +1,75 @@
 
 //html
 const html = require('html');
+//const html = require('html');
 const assert = require('assert');
 const http = require('http');
 const url = require('url');
 //File
 const fs = require('fs'); 
 const formidable = require('express-formidable');
- //mongodb
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
-const mongourl = '';
-const dbName = 'test';
-
-app.set('views', './views');
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(formidable());
-
 const express = require('express');
 const app = express();
 const session = require('cookie-session');
+const bodyParser = require('body-parser');
+ //mongodb
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const mongourl = 'mongodb+srv://mark:1234567!@cluster0.cmqjfdr.mongodb.net/?retryWrites=true&w=majority';
+const dbName = 'bookWell';
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(formidable());
+app.use(express.static('public'));
+
+//create new inventory docs
 
 
-
+//Middleware
+app.use(bodyParser.json());
 //cookies
 app.use(session({
     userid: "session",  
     keys: ['th1s!sA5ecretK3y'],
+    type: ""
     //maxAge: 90 * 24 * 60 * 60 * 1000
 }));
 //User Account
 var users = new Array(
 	{name: "mark", password: "mark"},
-    {name: "admin", password: "admin"},
     {name: "student", password: "student"}
 );
-
+var Admins = new Array(
+    {name:"Admin", password:"Admin"},
+    {name:"Mark", password:"Mark"}
+);
 app.get('/',(req,res) => {
 	if(!req.session.authenticated){
 		res.redirect("/login");
 	}
 	handle_Find(req, res, {});
 })
+//admin login methods
+app.get('/adminlogin', (req,res) => {
+    res.status(200).render("adminLogin.ejs");
+})
+app.post('/adminlogin', (req,res) => {
+    Admins.forEach((admin) => {
+		if (admin.name == req.fields.username && admin.password == req.fields.password) {
+        req.session.authenticated = true;
+        req.session.userid = req.fields.username;
+        req.session.type = "admin";
+        console.log(req.session.userid);
+        res.status(200).redirect("/home");
+        }
+    });
+    res.redirect("/");
+})
 
+//login methods
 app.get('/login', (req,res) => {
-	 
+    res.status(200).render("login.ejs");
 })
 app.post('/login', (req,res) =>{
     console.log("...Handling your login request");
@@ -53,10 +77,27 @@ app.post('/login', (req,res) =>{
 		if (user.name == req.fields.username && user.password == req.fields.password) {
         req.session.authenticated = true;
         req.session.userid = req.fields.username;
+        req.session.type = "user";
         console.log(req.session.userid);
         res.status(200).redirect("/home");
         }
     });
     res.redirect("/");
+})
+//check login status
+app.use((req, res, next) => {
+    console.log("...Checking login status");
+    if (req.session.authenticated && req.session.type =="user"){
+      next();
+    } else if(req.session.authenticated && req.session.type =="admin"){
+        res.redirect("/admin");
+    }
+    else {
+      res.redirect("/login");
+    }
+});
+//home page
+app.get('/home', (req,res) => {
+    res.status(200).render("admin.ejs");
 })
 app.listen(process.env.PORT || 8099);
